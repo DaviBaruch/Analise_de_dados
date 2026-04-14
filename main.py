7a65e563b0bb64c3f391973262b43def7c439d95
Davi Baruch <davibaruchg@gmail.com>
2026-04-14 03:25:14 -0300

    Criando um csv a partir do dicionario, e fazendo filtros de idade

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ dados = {
     'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba'],
     'Salario': [5000, 6000, 7000, 8000]
 
+}
+
+df = pd.DataFrame(dados)
+print(df)
+# Salvar o DataFrame em um arquivo CSV
+df.to_csv('dados.csv', index=False)
+df = pd.read_csv('dados.csv')
+print(df)
+idade = df['Idade']
 
 
 
