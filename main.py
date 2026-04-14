4e4e62cfd67f595d5a2ee1c1b22ca34331465fac
Davi Baruch <davibaruchg@gmail.com>
2026-04-14 03:23:43 -0300

    Criando pasta venv, e importando pandas e criando dados atravez de um dicionario

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+dados = {
+    'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Idade': [25, 30, 35, 40],
+    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba'],
+    'Salario': [5000, 6000, 7000, 8000]
+
+
+
+
+
+
+
