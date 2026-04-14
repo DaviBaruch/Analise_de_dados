09b091e1984191149d2f0ba2bd55953806f0aa8f
Davi Baruch <davibaruchg@gmail.com>
2026-04-14 03:29:59 -0300

    pegando um dataset do kaggle e ler esse csv

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import pandas as pd
-dados = {
-    'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Idade': [25, 30, 35, 40],
-    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba'],
-    'Salario': [5000, 6000, 7000, 8000]
 
-}
-
-df = pd.DataFrame(dados)
-print(df)
-# Salvar o DataFrame em um arquivo CSV
-df.to_csv('dados.csv', index=False)
-df = pd.read_csv('dados.csv')
-print(df)
-idade = df['Idade']
-
+df = pd.read_csv('most_viewed_videos_1000.csv')
+print(df.head())
 
 
 
