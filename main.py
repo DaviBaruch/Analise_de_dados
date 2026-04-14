b70caa0c130aa8d68443ac07e67d25c851a0ce94
Davi Baruch <davibaruchg@gmail.com>
2026-04-14 03:34:24 -0300

    analisando para ver senão tem nenhum valor nulo e fazendo um filtro dos top 10 videos mais assistidos

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 df = pd.read_csv('most_viewed_videos_1000.csv')
 print(df.head())
 
-
-
-
-
+print(df.info())
+df = df.dropna()
+print(df.info())
+top10_videos = df.sort_values(by='views', ascending=False).head(10)
+print(top10_videos[['title', 'views']])
